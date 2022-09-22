d795d887a30c13be300ad73c142bb3e71b98adc0
Kennerson Pereira <kennerson.pereira@thoughtworks.com>
2022-09-22 18:49:09 -0300

    create event

--- Events/VideoEncoder.cs
+++ Events/VideoEncoder.cs
@@ -10,10 +10,21 @@ namespace Events
         // 3 - Raise the event
 
         public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+        public event VideoEncodedEventHandler VideoEncoded;
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video...");
             Thread.Sleep(3000);
+            
+            OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, EventArgs.Empty);
+            }
         }
     }
 }
