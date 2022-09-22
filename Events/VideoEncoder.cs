e764fd1cb92a1284156af28cf27708d297301f3c
Kennerson Pereira <kennerson.pereira@thoughtworks.com>
2022-09-22 18:44:15 -0300

    create a delegate method

--- Events/VideoEncoder.cs
+++ Events/VideoEncoder.cs
@@ -5,6 +5,11 @@ namespace Events
 {
     public class VideoEncoder
     {
+        // 1 - Define a delegate
+        // 2 - Define an event based on that delegate
+        // 3 - Raise the event
+
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video...");
