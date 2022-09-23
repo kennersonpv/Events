798beedd7cb44084e41c62d9afc79ed925cd8bd3
Kennerson Pereira <kennerson.pereira@thoughtworks.com>
2022-09-23 16:12:47 -0300

    remove delegate and create a delegate by EventHandler

--- Events/VideoEncoder.cs
+++ Events/VideoEncoder.cs
@@ -8,9 +8,9 @@ namespace Events
         // 1 - Define a delegate
         // 2 - Define an event based on that delegate
         // 3 - Raise the event
-
-        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
-        public event VideoEncodedEventHandler VideoEncoded;
+        
+        // Create a delegate automatically
+        public event EventHandler<VideoEventArgs>  VideoEncoded;
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video...");
