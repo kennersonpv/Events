59eca69403cfc683218d061182c05cb42ff34019
Kennerson Pereira <kennerson.pereira@thoughtworks.com>
2022-09-23 15:58:16 -0300

    create MailService.cs

--- Events/Program.cs
+++ Events/Program.cs
@@ -4,12 +4,13 @@ namespace Events
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
             var video = new Video() { Title = "Video 1" };
-            var videoEncoder = new VideoEncoder();
-            
+            var videoEncoder = new VideoEncoder(); //publisher
+            var mailService = new MailService(); //subscriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
             videoEncoder.Encode(video);
         }
     }
